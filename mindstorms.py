aabb6d0b0d14973755fddb4a1c3841bc1d5f8f50
Nazanin <n.zinouri@gmail.com>
2017-12-17 21:29:27 -0500

    adding angies
    adding another instance of class Turtle

--- mindstorms.py
+++ mindstorms.py
@@ -5,7 +5,12 @@ def draw_square():
     window = turtle.Screen()
     window.bgcolor("red")
 
+    #brad is an instance of a class called Turtle
     brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("blue")
+    brad.speed(2)
+
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
@@ -14,6 +19,11 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
     brad.right(90)
+
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("yellow")
+    angie.circle(100)
 
     window.exitonclick()
 
